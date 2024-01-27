e0f84fdb2d9f0bdabe64220e4b7f43f27f242092
Sheldon Knuth <sheldon@abackbone.net>
2024-01-28 07:18:43 +0800

    Add type hints for public class methods, reformat code for cleaner view. TODO: Define URI class and GameAPI enums for type hints.

--- network.py
+++ network.py
@@ -10,8 +10,8 @@ import aiohttp
 import requests
 from dotenv import load_dotenv
 from zipfile import ZipFile
-from api import GameApi
-from storage import Storage
+from api import GameAPI
+from storage import Database, DiscordID
 from enum import Enum
 
 load_dotenv()
@@ -20,33 +20,36 @@ PASSWORD = os.environ["PASSWORD"]
 
 class NetworkManager:
     def __init__(self):
-        self.api = GameApi()
-        self.db = Storage()
+        self.api = GameAPI()
+        self.db = Database()
         self.logger = logging.getLogger('NetworkManager')
         self.logger.setLevel(logging.DEBUG)
         if not os.path.exists(CONFIG_DIR):
             os.mkdir(CONFIG_DIR)
 
     """ Log into the game by user's nutaku ID and update user database session. If session ID is given, bypass the login request."""
-    async def register(self, discord_user_id, session_id=None) -> None:
-        record = await self.db.user.get_user_record(discord_user_id)
+    async def register(self, discord_user_id: DiscordID, session_id=None) -> None:
+        user = await self.db.user.get_user(discord_user_id)
+        user_id = user.get_user_id()
+        prefix = user.get_prefix()
         if session_id:
-            me = await self._get_async(self.api.user.info, record['user_id'], session_id, record['user_id'][:3]).me()
-            if record['display_name']:
+            me = await self._get_async(self.api.user.info, user_id, session_id, prefix).me()
+            if user.get_name():
                 await self.db.user.update_session_id(discord_user_id, session_id)
             else:
-                await self.db.user.update_session(discord_user_id, me['user_id'], me['display_name'], session_id, socket_token, me['last_login_time'])
+                await self.db.user.update_user(discord_user_id, int(me['user_id']), me['display_name'], session_id, socket_token, int(me['last_login_time']))
             self.logger.info(f"(User: {discord_user_id}) has updated new session_id ({session_id})")
         else:
-            info = await self.login(discord_user_id, record['nutaku_id'], record['user_id'][:3])
+            nutaku_id = user.get_nutaku_id()
+            info = await self.login(discord_user_id, nutaku_id, prefix)
             if info:
-                await self.db.user.update_session(discord_user_id, info['user_id'], info['name'], info['session_id'], info['socket_token'], info['last_login_time'])
+                await self.db.user.update_user(discord_user_id, int(info['user_id']), info['name'], info['session_id'], info['socket_token'], int(info['last_login_time']))
                 self.logger.info(f"(User: {discord_user_id}) has updated new session_id ({info['session_id']})")
         # ensure that the new session has flushed into disk
         await asyncio.sleep(1)
 
     """ Send asynchronous POST login request and return player credentials on success. """
-    async def login(self, discord_user_id, nutaku_id, prefix) -> dict | None:
+    async def login(self, discord_user_id: DiscordID, nutaku_id: int, prefix: int) -> dict | None:
         acc = await self._post_async(self.api.auth.login.game_account, { "login_id": nutaku_id, "login_type": 0, "access_token": "", "pw": nutaku_id }, require_login=False)
         if acc.success():
             session_id = acc.response()['session_id']
@@ -135,23 +138,22 @@ class NetworkManager:
                     outfile.write(bytestream.getbuffer())
 
     """ Send asynchronous GET request by providing the API name and discord user ID. """
-    async def get(self, api_name, discord_user_id, q=None, event_id=None, battle_id=None) -> dict | None:
+    async def get(self, api_name: GameAPI, discord_user_id: DiscordID, q=None, event_id=None, battle_id=None) -> dict | None:
         uri = await self._fetch_uri(discord_user_id)
         user_id = uri['user_id']
         session_id = uri['session_id']
-        return await self._get_async(api_name, user_id, session_id, q, event_id, battle_id)
+        prefix = uri['prefix']
+        return await self._get_async(api_name, user_id, session_id, prefix, q, event_id, battle_id)
 
     """ Send asynchronous POST request by providing the API name and discord user ID. """
-    async def post(self, api_name, payload, discord_user_id, nutaku_id=None, require_login=True) -> dict | None:
+    async def post(self, api_name: GameAPI, payload: dict, discord_user_id: DiscordID, nutaku_id=None, require_login=True) -> dict | None:
         uri = await self._fetch_uri(discord_user_id, require_login)
         user_id = uri['user_id']
         session_id = uri['session_id']
         return await self._post_async(api_name, payload, user_id, session_id, nutaku_id, require_login)
 
     """ Async GET request builder. """
-    async def _get_async(self, api_name, user_id, session_id, q=None, event_id=None, battle_id=None) -> dict | None:
-        assert len(user_id) == 13
-        prefix = user_id[:3]
+    async def _get_async(self, api_name, user_id, session_id, prefix, q=None, event_id=None, battle_id=None) -> dict | None:
         if q and api_name == self.api.user.friend.search:
             uri = "{}q={}&user_id={}&session_id={}&server_prefix={}".format(api_name, q, user_id, session_id, prefix)
         elif event_id:
@@ -170,10 +172,7 @@ class NetworkManager:
                 return None
 
     """ Async POST request builder. """
-    async def _post_async(self, api_name, payload, user_id=None, session_id=None, nutaku_id=None, require_login=True) -> dict | None:
-        if user_id:
-            assert len(user_id) == 13
-            prefix = user_id[:3]
+    async def _post_async(self, api_name, payload, user_id=None, prefix=None, session_id=None, nutaku_id=None, require_login=True) -> dict | None:
         if require_login:
             async with aiohttp.ClientSession() as session:
                 uri = "{}user_id={}&session_id={}&server_prefix={}".format(api_name, user_id, session_id, prefix)
@@ -202,17 +201,23 @@ class NetworkManager:
                     self.logger.exception(f"POST request error: {e}")
                     return None
 
-    async def _fetch_uri(self, discord_user_id, require_login=True) -> dict:
-        record = await self.db.user.get_user_record(discord_user_id)
+    async def _fetch_uri(self, discord_user_id: DiscordID, require_login=True) -> dict:
+        user = await self.db.user.get_user(discord_user_id)
+        user_id = user.get_user_id()
+        prefix = user.get_prefix()
         if require_login:
-            if record['session_id']:
-                uri = { 'user_id': record['user_id'], 'prefix': record['user_id'][:3], 'session_id': record['session_id'], 'guild_id': record['guild_id'] }
+            session_id = user.get_session_id()
+            if session_id:
+                guild_id = user.get_guild_id()
+                #TODO: URI Class
+                uri = { 'user_id': user_id, 'prefix': prefix, 'session_id': session_id, 'guild_id': guild_id }
                 return uri
             else:
                 reason = f"(User: {discord_user_id}) has not logged into server yet!"
                 raise Exception(reason)
         else:
-            uri = { 'nutaku_id': record['nutaku_id'] ,'user_id': record['user_id'], 'prefix': record['user_id'][:3] }
+            nutaku_id = user.get_nutaku_id()
+            uri = { 'nutaku_id': nutaku_id, 'user_id': user_id, 'prefix': prefix }
             return uri
 
     def _get_asset_list(self) -> (str, list):
@@ -267,6 +272,3 @@ class Response(dict):
     def __str__(self) -> str:
         fmt = json.dumps(self.body, indent=2, ensure_ascii=False)
         return f"{fmt}"
-
-
-NetworkManager()
